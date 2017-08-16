cb3191183f7cc2f462f706a61b9f03884255a7eb
Xing Yao <yaoxing1990@gmail.com>
2017-08-16 16:14:22 -0500

    Initial commit

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+    <title>@yield('title')</title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ URL::to('css/main.css') }}">
+</head>
+<body>
+    @include('includes.header')
+    <div class="container">
+        @yield('content')
+    </div>
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script src="https://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <script src="{{ URL::to('js/main.js') }}"></script>
+</body>
+</html>
