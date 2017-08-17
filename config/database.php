86a30a30f827ebdd36be617bae12294a59379ad4
Xing Yao <yaoxing1990@gmail.com>
2017-08-17 13:03:33 -0500

    Fix bugs of Account page

--- config/database.php
+++ config/database.php
@@ -48,8 +48,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host'      => RDS_HOSTNAME,
             'port' => env('DB_PORT', '3306'),
+//            'host' => env('DB_HOST', '127.0.0.1'),
+//            'database'  => env('DB_name', 'laravel_basics'),
+//            'username'  => env('DB_USERNAME', 'root'),
+//            'password'  => env('DB_PASSWORD', 'root'),
+            'host'      => RDS_HOSTNAME,
             'database'  => RDS_DB_NAME,
             'username'  => RDS_USERNAME,
             'password'  => RDS_PASSWORD,
